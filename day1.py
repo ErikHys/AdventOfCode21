34200df75fb4de21b7e05557ec354440b5037e74
ErikHys <60257108+ErikHys@users.noreply.github.com>
2021-12-01 13:12:10 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,7 +1,6 @@
 with open("inputs/input_day_1.txt") as file:
     depths = [int(x) for x in file.read().split('\n')[:-1]]
 
-# depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 # part 1
 c = 0
 for i in range(1, len(depths)):
@@ -14,10 +13,6 @@ print(c)
 c = 0
 for i in range(3, len(depths)):
     if sum(depths[i-3:i]) < sum(depths[i-2:i+1]):
-        # print(sum(depths[i - 4:i - 1]), sum(depths[i - 3:i]), "passed")
         c += 1
-    else:
-        # print(sum(depths[i - 4:i - 1]), sum(depths[i - 3:i]), "failed")
-        pass
 
 print(c)
